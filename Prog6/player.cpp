763944602edc4622bf1c82b1a0363b1402d540c9
bergvall95 <36799924+bergvall95@users.noreply.github.com>
2018-11-05 14:45:43 -0600

    Add files via upload

--- Prog6/player.cpp
+++ Prog6/player.cpp
@@ -1,5 +1,98 @@
-//
-// Created by Naris on 11/1/2018.
-//
-
+// Player.cpp written by Alex and Naris
+// 11/5/18
+#include <iostream>
+#include <cstdlib>
+#include "deck.h"
+#include "card.h"
 #include "player.h"
+
+
+void player::addCard(card c) { //adds a card to the hand
+    myHand.push_back(c);
+
+}
+
+void player::bookCards(card c1, card c2){
+    player::myBook.push_back(player::removeCardFromHand(c1););
+    player::myBook.push_back(player::removeCardFromHand(c2););
+}
+
+//OPTIONAL
+// comment out if you decide to not use it
+//this function will check a players hand for a pair.
+//If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
+
+bool player::checkHandForBook(card &c1, card &c2){
+    if (c1.getRank()==c2.getRank()){
+        return (true);
+    }
+    else{return (false);}
+}
+
+//OPTIONAL
+// comment out if you decide to not use it
+//Does the player have a card with the same rank as c in her hand?
+bool player::rankInHand(card c) const{
+    vector<card>::const_iterator iter;
+
+    for(iter=myHand.begin(); iter!=myHand.end(); iter++){
+        if((*iter).getRank() ==c.getRank()){ // if the rank of the card in the hand is equal to the rank of the card given to the function, return true
+            return true;
+        }
+    }
+    return false;
+}
+
+//uses some strategy to choose one card from the player's
+//hand so they can say "Do you have a 4?"
+card player::chooseCardFromHand() const{
+
+}
+
+//Does the player have the card c in her hand?
+bool player::cardInHand(card c) const{
+
+}
+
+//Remove the card c from the hand and return it to the caller
+card player::removeCardFromHand(card c){
+    vector<card>::const_iterator iter;
+
+    for(iter=myHand.cbegin();iter!=myHand.cend(); iter++){
+        if((*iter).getRank() == c.getRank()){ // if the rank of the card in the hand is equal to the rank of the card given to the function, return true
+            myHand.erase(iter);
+        }
+    }
+}
+
+string player::showHand() const{
+
+}
+string player::showBooks() const{
+
+}
+
+int player::getHandSize() const{
+    return(myHand.size());
+}
+
+int  player::getBookSize() const{
+    return(myBook.size());
+}
+//OPTIONAL
+// comment out if you decide to not use it
+//this function will check a players hand for a pair.
+//If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
+
+bool player::checkHandForPair(card &c1, card &c2){
+
+}
+
+//OPTIONAL
+// comment out if you decide to not use it
+//Does the player have a card with the same rank as c in her hand?
+//e.g. will return true if the player has a 7d and the parameter is 7c
+
+bool player::sameRankInHand(card c) const{
+
+}
--- Prog6/player.h
+++ Prog6/player.h
@@ -1,14 +1,88 @@
-//
-// Created by Naris on 11/1/2018.
-//
+// FILE:  player.h
+// written by Roger Priebe
+// 1/22/08 (revised 9/2/08)
+// This class represents a player in a card game that takes "tricks"
+// The "Books" represent a container for holding tricks
 
-#ifndef PROG6_PLAYER_H
-#define PROG6_PLAYER_H
+#ifndef _PLAYER_H
+#define _PLAYER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
 
 
-class player {
+#include "card.h"
+using namespace std;
+
+
+class player
+{
+public:
+    player();
+
+    player(string name) {
+        myName = name;
+    }
+
+    string getName() const {
+        return myName;
+    }
+
+    void addCard(card c);  //adds a card to the hand
+    void bookCards(card c1, card c2);
+
+    //OPTIONAL
+    // comment out if you decide to not use it
+    //this function will check a players hand for a pair.
+    //If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
+
+    bool checkHandForBook(card &c1, card &c2);
+
+    //OPTIONAL
+    // comment out if you decide to not use it
+    //Does the player have a card with the same rank as c in her hand?
+    bool rankInHand(card c) const;
+
+    //uses some strategy to choose one card from the player's
+    //hand so they can say "Do you have a 4?"
+    card chooseCardFromHand() const;
+
+    //Does the player have the card c in her hand?
+    bool cardInHand(card c) const;
+
+    //Remove the card c from the hand and return it to the caller
+    card removeCardFromHand(card c);
+
+    string showHand() const;
+    string showBooks() const;
+
+    int getHandSize() const;
+    int getBookSize() const;
+
+    //OPTIONAL
+    // comment out if you decide to not use it
+    //this function will check a players hand for a pair.
+    //If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
+
+    bool checkHandForPair(card &c1, card &c2);
+
+    //OPTIONAL
+    // comment out if you decide to not use it
+    //Does the player have a card with the same rank as c in her hand?
+    //e.g. will return true if the player has a 7d and the parameter is 7c
+
+    bool sameRankInHand(card c) const;
+
+
+private:
+
+    vector <card> myHand;
+    vector <card> myBook;
+
+    string myName;
 
 };
 
 
-#endif //PROG6_PLAYER_H
+#endif
